a724fef8922cebfc5f95f3e5c6a730592296bd66
Alex <186313861+atbu@users.noreply.github.com>
2025-08-09 20:01:43 +0100

    Fix build error

--- frontend/src/views/auth/RegisterView.vue
+++ frontend/src/views/auth/RegisterView.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import RegisterForm from "@/components/RegisterForm.vue";
+import RegisterForm from "@/components/forms/RegisterForm.vue";
 </script>
 
 <script lang="ts">
